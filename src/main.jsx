024a4b4dde76a814dc7aa1a6394fd7d5094f8de3
Mario Marquestó <mariomarquesto@gmail.com>
2026-06-10 17:30:30 -0300

    initial 19

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from "./components/ScrollToTop.jsx";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
+
       <App />
     </BrowserRouter>
   </React.StrictMode>
